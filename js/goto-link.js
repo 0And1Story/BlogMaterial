122352f76c9d2c478b69fb34fb5b17a9b7f7783d
0And1Story <witherman233@gmail.com>
2019-08-31 15:05:06 +0800

    First upload

---
+++ js/goto-link.js
@@ -0,0 +1,18 @@
+function gotoLink(link) {
+    let main = document.getElementsByTagName('main')[0];
+    main.setAttribute('style', 'animation: fade-out 1s ease-out; opacity: 0; position: relative; top: 50px;');
+    setTimeout(function() { window.location.href = link; }, 1000);
+}
+
+function initLink() {
+    let links = document.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i ++) {
+        if (!links[i].getAttribute('href')) continue;
+        let href = links[i].getAttribute('href');
+        links[i].setAttribute('href', 'javascript: gotoLink(\'' + href + '\');');
+    }
+}
+
+(function() {
+    window.addEventListener('load', initLink, false);
+})();
